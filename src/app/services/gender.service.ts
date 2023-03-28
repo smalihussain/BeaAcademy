d6b63f48fba10adbf5d7baf74c47850359e059e2
Syed Muhammad Ali Hussain <smalihussain@hotmail.com>
2023-03-28 21:06:35 +0800

    First Commit

---
+++ src/app/services/gender.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Gender } from '../models/api-models/gender.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenderService {
+
+  private baseApiUrl = environment.baseApiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getGenderList(): Observable<Gender[]> {
+    return this.httpClient.get<Gender[]>(this.baseApiUrl + '/genders');
+  }
+}
